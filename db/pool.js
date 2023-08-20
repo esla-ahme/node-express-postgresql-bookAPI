605d85cfc4ed3b9bd90d29f9ab399eda396b1933
Eslam <eslamahmed9861@gmail.com>
2023-08-20 20:35:36 +0300

    1- configured winston to auto delete files after some time 2- added loggert to db connection

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const { Pool } = require("pg");
+const Logger = require("../services/logger");
+const logger = new Logger("dbPool");
 
 const db_config = {
   connectionString: process.env.DATABASE_URL,
@@ -8,5 +10,16 @@ const db_config = {
   idleTimeoutMillis: 200,
 };
 const pool = new Pool(db_config);
+
+pool.on("connect", () => {
+  logger.info("connected to database");
+});
+pool.on("error", (err) => {
+  logger.error("database connection error", err);
+});
+pool.on("remove", () => {
+  logger.info("client removed");
+});
+
 
 module.exports = pool;
--- services/logger.js
+++ services/logger.js
@@ -5,15 +5,36 @@
  */
 
 const winston = require("winston");
+const winstonDaily = require("winston-daily-rotate-file");
 const dotenv = require("dotenv");
 dotenv.config();
 // timestamp + level + message
 const dateFormat = () => {
   return new Date(Date.now()).toLocaleString();
 };
+
+/*
+  const oldTransport= new winston.transports.File({
+          filename: `${process.env.LOG_FILE_PATH}/${route}.log`,
+        })
+const newTransport = new winstonDaily({
+      filename: `${process.env.LOG_FILE_PATH}/${route}/%DATE%.log`,
+      datePattern: "YYYY-MM-DD-HH-mm",
+      zippedArchive: true, // zip old files
+      maxSize: "5m", // 5mb per file
+      maxFiles: "7d", //minutes
+    });
+        */
 class LoggerService {
   constructor(route) {
     this.route = route;
+    this.transport = new winstonDaily({
+      filename: `${process.env.LOG_FILE_PATH}/${route}/%DATE%.log`,
+      datePattern: "YYYY-MM-DD-HH-mm",
+      zippedArchive: true, // zip old files
+      maxSize: "5m", // 5mb per file
+      maxFiles: "7d", //minutes
+    });
     this.logger = winston.createLogger({
       level: "info",
       format: winston.format.printf((info) => {
@@ -25,10 +46,8 @@ class LoggerService {
       }),
 
       transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({
-          filename: `${process.env.LOG_FILE_PATH}/${route}.log`,
-        }),
+        // new winston.transports.Console(),
+        this.transport,
       ],
     });
   }
